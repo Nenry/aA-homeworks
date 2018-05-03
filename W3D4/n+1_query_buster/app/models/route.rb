abd2609d6c3143fd3662840746caabe722bff607
HN <hhnguyen92@gmail.com>
2018-05-02 19:40:52 -0700

    Complete better_drivers_query

--- W3D4/n+1_query_buster/app/models/route.rb
+++ W3D4/n+1_query_buster/app/models/route.rb
@@ -30,6 +30,20 @@ class Route < ApplicationRecord
   end
 
   def better_drivers_query
-    # TODO: your code here
+    #Create a hash with bus id's as keys and an array of
+    #bus drivers as the corresponding value.
+    all_drivers = {}
+    all_buses = self.buses.includes(:drivers)
+
+    all_buses.each do |bus|
+      drivers = []
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+
+      all_drivers[bus.id] = drivers
+    end
+
+    all_drivers
   end
 end
